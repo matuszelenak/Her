6026d000bc089f9d4056af343982542fcb09f3ab
Matus Zelenak <matuszelenak@protonmail.com>
2025-07-10 00:15:35 +0200

    Nuke the React UI

--- ui/src/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const axiosDefault = axios.create({
-    baseURL: `${window.location.protocol}//${window.location.host}/api`,
-    headers: {
-        'Content-Type': 'application/json'
-    },
-})
--- ui/src/utils/vad/messages.ts
+++
@@ -1,9 +0,0 @@
-export enum Message {
-    AudioFrame = "AUDIO_FRAME",
-    SpeechStart = "ASSISTANT_SPEECH_START",
-    VADMisfire = "VAD_MISFIRE",
-    SpeechRealStart = "SPEACH_REAL_START",
-    SpeechEnd = "SPEECH_END",
-    SpeechStop = "SPEECH_STOP",
-    SpeechContinue = "SPEECH_CONTINUE",
-}
